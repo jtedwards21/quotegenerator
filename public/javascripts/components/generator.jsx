eb95a8c692dfd9f0e66baef8642bc4ab1bed4e9c
James Ahlm <14782828428@qq.com>
2016-12-02 10:01:20 +0800

    Added Button Functionality

--- public/javascripts/components/generator.jsx
+++ public/javascripts/components/generator.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 
 export default class Generator extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.quotes = {
+  	"Albert Einstein":"Insanity: doing the same thing over and over again and expecting different results.",
+  	"Jimmy Dean":"I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+  	"Audrey Hepburn":"Nothing is impossible, the word itself says 'I'm possible'!"
+    };
 
     this.state = {
+      author: "",
+      quote: ""
     };
+    this.changeQuote.bind(this);
+  }
+  getRand(){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  changeQuote(){
+    var keys = Object.keys(this.quotes);
+    var rand = getRand(0, keys.length - 1);
+    this.setState({quote: this.quotes[keys[rand]]});
+    this.setState({author: keys[rand]});
+  }
+  componentDidMount(){
+    this.changeQuote();
   }
   render() {
     return (
 	　　<div className="quote-box">
 	    <div className="title">Classic Quotes</div>
-	　　　　<span className="quote">Insanity: doing the same thing over and over again and expecting different results.</span>
-    　　　　　　　　<span className="author">Albert Einstein</span>
-	    <div className="img-container"><img src="img/switch.png" id="quote-btn" className="switch" /></div>
+	　　　　<span className="quote">{this.state.quote}</span>
+    　　　　　　　　<span className="author">{this.state.author}</span>
+	    <div className="img-container"><img onClick={this.changeQuote} src="img/switch.png" id="quote-btn" className="switch" /></div>
   　　　　　　　　</div>
     );
   }
